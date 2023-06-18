d18af31fe92f942b88365f50dc7b5dc62e6f0517
maxtroezra <maestroezra04@gmail.com>
2023-06-18 18:41:21 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/aplikasitest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aplikasitest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ProfilePage());
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Profile'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: const AssetImage('usro.jpg'),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Ezra',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                'Software Engineer',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.email),
+                  SizedBox(width: 8.0),
+                  Text('maestroezra04@gmail.com'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.phone),
+                  SizedBox(width: 8.0),
+                  Text('085240366290'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.location_on),
+                  SizedBox(width: 8.0),
+                  Text('Sulawesi Utara, Manado'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
